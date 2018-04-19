f853819ab24484acbbc6dcf286a7df98c7645b29
logon84 <logon84@users.noreply.github.com>
2018-04-19 21:39:36 +0200

    Add files via upload

---
+++ misc/mnt/jffs2/startup.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+
+#Put here user custom scripts to launch at boot. This script can be modified using dropbear ssh server to alow further mods without needing to flash a new rootfs into the ONT.
