8fd144125f119195e84706a1f4f8eca349d0bd55
logon84 <logon84@users.noreply.github.com>
2018-04-20 12:39:21 +0200

    Update startup.sh

--- misc/mnt/jffs2/startup.sh
+++ misc/mnt/jffs2/startup.sh
@@ -1,3 +1,6 @@
 #/bin/sh
 
-#Put here user custom scripts to launch at boot. This script can be modified using dropbear ssh server to alow further mods without needing to flash a new rootfs into the ONT.
+#This script will launch at every boot.
+#This script can be modified using vi thru shell (dropbear).
+#This script allows further ONT mods without needing to flash a new rootfs.
+echo "Hello from JFFS2 World, Mr. RootFS"
